618c03bd9a10b4fd872e2cd5ae01ffbd34edcfbc
lunabear <dongom910@gmail.com>
2022-05-16 18:46:13 +0900

    drawal 구현

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,86 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('appbar icon menu'),
+        title: Text('appbar icon menu'),
         centerTitle: true,
         elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print('shopping cart button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('search cart button is clicked');
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        //drawer을 넣으면 햄버거 메뉴 위젯이 자동으로 생성이되고
+        //좌에서 우로 펼쳐지는 위젯이 생성됨
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/imo1.PNG'),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: <Widget>[
+                //다른계정 이미지??
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('assets/imo2.PNG'),
+                ),
+              ],
+              accountName: Text('dongom'),
+              accountEmail: Text('dongom910@gmail.com'),
+              onDetailsPressed: () {
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40.0),
+                      bottomRight: Radius.circular(40.0))), //상세보기 이벤트 및 버튼
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Colors.grey[850]),
+              title: Text('HOME'),
+              onTap: () {
+                print('home is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: Text(
+                'Settings',
+              ),
+              onTap: () {
+                print('settings is clicked');
+              },
+              trailing: Icon(
+                Icons.add,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer),
+              title: Text('Q&A'),
+              onTap: () {
+                print('Q&A is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+          ],
+        ),
       ),
     );
   }
